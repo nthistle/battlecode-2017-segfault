b417de453ee4f853580c81d5c63215356866d0b0
Neil Thistlethwaite <neilt1111@yahoo.com>
2017-01-15 18:09:05 -0500

    add dead circlescout code

--- dynamicstrategy/RobotPlayer.java
+++ dynamicstrategy/RobotPlayer.java
@@ -45,8 +45,10 @@ public strictfp class RobotPlayer {
     	int preID = rc.readBroadcast(500) + 1;
     	rc.broadcast(500, preID);
     	float[] center = unpack(rc.readBroadcast(0));
-    	float myDist = rc.getLocation().distanceSquaredTo(new MapLocation(center[0],center[1]))*100;
-    	System.out.println("My dist to center: " + myDist);
+    	System.out.println("Read center as " + center[0] + ", " + center[1]);
+    	float myDist = rc.getLocation().distanceSquaredTo(new MapLocation(center[0],center[1]));
+    	System.out.println("My coords: " + rc.getLocation().x + ", " + rc.getLocation().y);
+    	System.out.println("PreID: " + preID + ", My dist to center^2: " + myDist);
     	rc.broadcast(500+preID, (int)(myDist));
     	Clock.yield();
     	// T=2
@@ -59,10 +61,23 @@ public strictfp class RobotPlayer {
     	System.out.println("Self-identified as rank #" + rank);
     	boolean isAlpha = (rank == 0);
     	if(isAlpha) {
-    		System.out.println("I am the Alpha");
+    		System.out.println("I am the Alpha, broadcasting my coords");
     		rc.broadcast(1, pack(rc.getLocation().x,rc.getLocation().y));
     	}
+    	
     	Clock.yield();
+    	if(isAlpha) {
+    		Direction dir = randomDirection();
+    		for(int i = 0; i < 10 && !rc.canBuildRobot(RobotType.GARDENER, dir); i++) {
+    			dir = randomDirection();
+    		}
+    		if(rc.canBuildRobot(RobotType.GARDENER, dir)) {
+    			rc.buildRobot(RobotType.GARDENER, dir);
+    		}
+    		Clock.yield();
+    		while(true)
+    			Clock.yield();
+    	}
     }
     
     /*
@@ -88,25 +103,119 @@ public strictfp class RobotPlayer {
     	return new float[] {netX/initArchonLocs.length, netY/initArchonLocs.length};
     }
     
-    static void runGardener() {
-    	// pass
+    static void runGardener() throws GameActionException {
+    	while(true) {
+    		Direction dir = randomDirection();
+    		for(int i = 0; i < 10 && !rc.canBuildRobot(RobotType.SCOUT, dir); i++) {
+    			dir = randomDirection();
+    		}
+    		if(rc.canBuildRobot(RobotType.SCOUT, dir)) {
+    			rc.buildRobot(RobotType.SCOUT, dir);
+        		while(true)
+        			Clock.yield();
+    		}
+    		Clock.yield();
+    	}
     }
     
-    static void runScout() {
-    	// pass
+    static void runScout() throws GameActionException {
+    	//circleScout(); // todo: only if "alpha"/initial scout
     }
+    
+    
+    
+    // TODO: repurpose circle scout code
+    
+    /*
+    
+    static void circleScout() throws GameActionException {
+    	float[] alphalocation = unpack(rc.readBroadcast(1));
+    	MapLocation al = new MapLocation(alphalocation[0], alphalocation[1]);
+		//get sufficient distance away from archon
+    	float[] centerlocation = unpack(rc.readBroadcast(0));
+    	MapLocation cl = new MapLocation(centerlocation[0], centerlocation[1]);
+    	MapLocation myLoc = rc.getLocation();
+    	Direction dir = myLoc.directionTo(cl);
+    	float mdist = myLoc.distanceTo(al);
+    	System.out.println("Circlescout, about to move away");
+    	while(mdist < 10) { // circle radius @17
+    		System.out.println("Still in loop");
+    		if(mdist > 14.5) {
+    			if(rc.canMove(dir, 17.0f-mdist)) {
+    				rc.move(dir, 17.0f-mdist);
+    			}
+    		}
+    		else if(rc.canMove(dir)) {
+    			rc.move(dir);
+    		}
+    		
+    		Clock.yield();
+    		myLoc = rc.getLocation();
+    		mdist = myLoc.distanceTo(al);
+    	}
+    	
+    	
+    	for(int i = 0; i < 43; i ++) {
+    		myLoc = rc.getLocation();
+    		dir = myLoc.directionTo(al);
+    		dir = dir.rotateRightDegrees(85.78f); // with stride radius 2.5, this keeps in circle
+    		if(rc.canMove(dir)) {
+    			rc.move(dir);
+    		}
+    		else {
+    			System.out.println("Cannot move (circle) in dir " + dir);
+    		}
+    		Clock.yield();
+    	}
+    	// guaranteed circle radius @ almost exactly 17.0f
+    	
+    	
+    	// for now, perpetually circle
+    	
+		//circle around archon
+		float theta = 7.5f; // 48 * 7.5 = 360 degrees
+		Direction alphaToScout = al.directionTo(rc.getLocation());
+		MapLocation newLoc = rc.getLocation();
+		int[] totaltrees = new int[48];
+		int sumtrees = 0;
+		for (int i=0; i<48; i++) {
+			//sense trees (a simplistic approach)
+			if (rc.onTheMap(rc.getLocation(), 10.0f)) {
+    			TreeInfo[] mytrees = rc.senseNearbyTrees();
+    			sumtrees += mytrees.length;
+    			totaltrees[i] = mytrees.length;
+    			System.out.println(mytrees.length);
+			}
+			// move scout
+			alphaToScout = alphaToScout.rotateRightDegrees(theta);
+			System.out.println(alphaToScout);
+			newLoc = al.add(alphaToScout, 19.0f);
+			//make sure this is less than 2.5, but it should be
+			if (rc.canMove(newLoc)) {
+				System.out.println("MOVING BRUH");
+				System.out.println(newLoc);
+				rc.move(newLoc);
+			} else {
+				System.out.println("RIP CANT MOVE THIS SHOULD NEVER HAPPEN UNLESS WE REACH THE EDGE");
+			}
+            Clock.yield();
+		}
+		rc.broadcast(151, (int)sumtrees);
+    }
+    */
+    
     
     // ====================== HELPER =======================
     
     
     public static int pack(float x, float y) {
-		return (int)((((int)(x*100))/100.0*10000000) + (int)(((int)(y*100))/100.0*100));
+		return (int)((((int)(x*10))/10.0*100000) + (int)(y*10));
 	}
     
 	public static float[] unpack(int p) {
 		float[] ret = new float[2];
-		ret[0] = ((p / 100000) / 100.0f);
-		ret[1] = ((p % 100000) / 100.0f);
+		ret[0] = ((p / 10000) / 10.0f);
+		ret[1] = ((p % 10000) / 10.0f);
 		return ret;
 	}
 
