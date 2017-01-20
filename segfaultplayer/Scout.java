c5bd0b9d02fa13ddcb432e0bd183941dfbd030a4
nikhilsardana <2018nsardana@tjhsst.edu>
2017-01-20 16:41:42 -0500

    add harass

--- segfaultplayer/Scout.java
+++ segfaultplayer/Scout.java
@@ -1,4 +1,6 @@
 package segfaultplayer;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.HashSet;
 
 import java.util.Set;
@@ -17,8 +19,64 @@ public strictfp class Scout extends RobotBase
 		MapLocation dank = rc.getInitialArchonLocations(enemy)[0];
 		System.out.println(dank);
 		float dist = pathscout(dank);
-		circleEnemy(dank, dist);
+		harass();
+		//circleEnemy(dank, dist);
 	}
+	public void harass() throws GameActionException {
+		RobotType[] orderedBots = {RobotType.SCOUT, RobotType.GARDENER, RobotType.LUMBERJACK, RobotType.SOLDIER, RobotType.ARCHON};
+		while(true) {
+			RobotInfo[] iKillYou = pickRobot(orderedBots);
+			RobotInfo bestRobot = iKillYou[0];
+			int id = bestRobot.ID;
+			while(bestRobot.health!=0) {
+				MapLocation myLoc = rc.getLocation();
+				Direction bestDir = myLoc.directionTo(bestRobot.location);
+				// shoot at enemy
+				if(isSingleShotClear(bestDir, true)) {
+					if(rc.canFireSingleShot()) {
+						rc.fireSingleShot(bestDir);
+					}
+				} 
+				if(rc.canMove(bestDir)) {
+					rc.move(bestDir); //move towards enemy
+				}
+				Clock.yield();
+				// re-sense enemy info
+				if(rc.canSenseRobot(id)) { 
+					bestRobot = rc.senseRobot(id);
+				}
+			}
+		}
+	}
+	public RobotInfo[] pickRobot(RobotType[] mybots) throws GameActionException {
+		MapLocation myLoc = rc.getLocation();
+		RobotInfo[] nearbyRobots = rc.senseNearbyRobots(rc.getType().sensorRadius, enemy);
+	    Arrays.sort(nearbyRobots, new Comparator<RobotInfo>() {
+	        public int compare(RobotInfo r1, RobotInfo r2) {
+	        	int type1 = 0;
+	        	int type2 = 0;
+	        	for (int i=0; i<mybots.length; i++) {
+	        		if (r1.type==mybots[i]) {
+	        			type1 = i;
+	        		}
+	        		if(r2.type==mybots[i]) {
+	        			type2 = i;
+	        		}
+	        	}
+	        	if (type1==type2) {
+	        		return (Float.compare(myLoc.distanceTo(r1.location), myLoc.distanceTo(r2.location)));
+	        	} else {
+	        		return Integer.compare(type1, type2);
+	        	}	
+	        }
+	    });
+	    for (int i=0; i<nearbyRobots.length; i++) {
+	    	System.out.println(nearbyRobots[i].type);
+	    	System.out.println(myLoc.distanceTo(nearbyRobots[i].location));
+	    }
+	    return nearbyRobots;
+	}
+	
 	public void circleEnemy(MapLocation el, float circlerad) throws GameActionException {
 		float theta = 2.5f/circlerad; // 48 * 7.5 = 360 degrees
 		MapLocation myLocation = rc.getLocation();
@@ -87,6 +145,8 @@ public strictfp class Scout extends RobotBase
 					rc.move(toEnemy);
 					steps+=1;
 					rc.broadcast(150, steps);
+				} else {
+					return rc.getLocation().distanceTo(el);
 				}
 			}
 			Clock.yield();
