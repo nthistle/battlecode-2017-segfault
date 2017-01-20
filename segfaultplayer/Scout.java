041ee4ce9fa48302bac98fa7cfb422187b83185e
nikhilsardana <2018nsardana@tjhsst.edu>
2017-01-20 13:01:50 -0500

    add scout path + tree data

--- segfaultplayer/Scout.java
+++ segfaultplayer/Scout.java
@@ -1,4 +1,7 @@
 package segfaultplayer;
+import java.util.HashSet;
+import java.util.Set;
+
 import battlecode.common.*;
 
 
@@ -10,7 +13,55 @@ public strictfp class Scout extends RobotBase
 	}
 	
 	public void run() throws GameActionException {
-
+		MapLocation el = rc.getInitialArchonLocations(enemy)[0];
+		pathscout(el);
+	}
+	
+	public void pathscout(MapLocation el) throws GameActionException {
+		Set<TreeInfo> uniqueTrees = new HashSet<TreeInfo>();
+		float scaledNumTrees = 0;
+		int steps = 0;
+		
+		while (rc.getLocation().distanceTo(el) < 1.0) {
+			boolean hasMoved = false;
+			MapLocation myLoc = rc.getLocation();
+			float distance = myLoc.distanceTo(el);
+			TreeInfo[] myTrees = rc.senseNearbyTrees();
+			// add all the new trees
+			for (TreeInfo k : myTrees) {
+				if(uniqueTrees.add(k)) {
+					scaledNumTrees+=(k.getRadius()*k.getRadius());
+				}
+			}
+			//broadcast tree data
+			rc.broadcast(151, uniqueTrees.size());
+			rc.broadcast(152, (int)scaledNumTrees);
+			
+			// finds and moves to closest tree with bullets in the direction of the enemy
+			for (TreeInfo k : myTrees) {
+				MapLocation treeLocation = k.getLocation();
+				if(k.containedBullets!=0) {
+					if(treeLocation.distanceTo(el) < distance) {
+						if(rc.canMove(treeLocation)) {
+							rc.move(treeLocation);
+							hasMoved = true;
+							steps+=1;
+							rc.broadcast(150, steps);
+							break;
+						}
+					}
+				}
+			}
+			if (!hasMoved) {
+				Direction toEnemy = myLoc.directionTo(el);
+				if (rc.canMove(toEnemy)) {
+					rc.move(toEnemy);
+					steps+=1;
+					rc.broadcast(150, steps);
+				}
+			}
+			Clock.yield();
+		}
 	}
 
 	//scout linear move
