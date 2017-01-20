ef2cb4f9ab52be2aad8dc883e714bb86f56c22b8
Neil Thistlethwaite <neilt1111@yahoo.com>
2017-01-20 16:11:02 -0500

    work on assignalpha

--- segfaultplayer/CommunicationsHandler.java
+++ segfaultplayer/CommunicationsHandler.java
@@ -14,11 +14,24 @@ public final strictfp class CommunicationsHandler
 	// add some communications stuff
 	
 	public int assignAlphaArchonProtocol(Archon a) throws GameActionException {
+		return assignAlphaArchonProtocol(a, false);
+	}
+	
+	public int assignAlphaArchonProtocol(Archon a, boolean debugPrint) throws GameActionException {
 		// turn one, calculate distance to nearest archon
 		MapLocation[] enemyStartingArchons = a.rc.getInitialArchonLocations(a.enemy);
-		
-		
+		float myDist = RobotBase.findClosest(a.rc.getLocation(), enemyStartingArchons).distanceTo(a.rc.getLocation());
+		if(debugPrint)
+			System.out.println("[ID" + a.getID() + "] found my distance as " + myDist); 
+		a.rc.broadcast(200 + a.getID(), (int)(100*myDist));
+		Clock.yield();
 		
 		return 1;
+	}
+	
+	
+	public int getNumMade(RobotController rc, RobotType rt) throws GameActionException {
+		int slot = RobotBase.typeToNum(rt);
+		return rc.readBroadcast(100+slot);
 	}
 }
