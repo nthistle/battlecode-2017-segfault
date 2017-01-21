a0fb333018b4954cf7520624d0d687fca411b3f4
Neil Thistlethwaite <neilt1111@yahoo.com>
2017-01-21 00:20:26 -0500

    add basic gardener logic

--- segfaultplayer/Gardener.java
+++ segfaultplayer/Gardener.java
@@ -12,7 +12,7 @@ public strictfp class Gardener extends RobotBase
 	}
 	
 	public void run() throws GameActionException {
-		boolean xd = true;
+		/*boolean xd = true;
 		while(true) {
 			Direction dir = randomDirection();
 			if(rc.canBuildRobot(RobotType.SCOUT,dir)==true && 1==0)
@@ -30,6 +30,63 @@ public strictfp class Gardener extends RobotBase
 				rc.move(dir);
 
 			Clock.yield();
+		}*/
+	}
+	
+	/**
+	 * Creates a full hex pod wherever it is when called, blocking in all 6 sides,
+	 * uses up bullets as soon as it can get them (50+)
+	 */
+	public void makeHexPod() throws GameActionException {
+		float smallAngleInc = (float)Math.PI/16.0f;
+		float treeOffsetAngle = (float)Math.PI/3.0f;
+		while(rc.getTeamBullets() < RobotType.GARDENER.bulletCost) {
+			waterLowest();
+			Clock.yield();
+		}
+		// now we can afford a gardener
+		float cAngle = 0.0f;
+		while(cAngle < 2.0*(float)Math.PI && !rc.canPlantTree(new Direction(cAngle)))
+			cAngle += smallAngleInc;
+		if(!rc.canPlantTree(new Direction(cAngle)))
+			return; // hex pod failed
+		boolean[] planted = new boolean[] {true, false, false, false, false, false};
+		rc.plantTree(new Direction(cAngle));
+		waterLowest();
+		Clock.yield();
+		for(int i = 0; i < 5; i ++) {
+			while(rc.getTeamBullets() < RobotType.GARDENER.bulletCost) {
+				waterLowest();
+				Clock.yield();
+			}
+			if(rc.canPlantTree(new Direction(cAngle + (i+1) * treeOffsetAngle))) {
+				planted[i+1] = true;
+				rc.plantTree(new Direction(cAngle + (i+1) * treeOffsetAngle));
+				waterLowest();
+				Clock.yield();
+			}
 		}
 	}
+    
+	
+	
+	
+	
+	
+	
+    private void waterLowest() throws GameActionException {
+		TreeInfo[] myTrees = rc.senseNearbyTrees(2.0f, rc.getTeam());
+
+		if (myTrees.length > 0) { // Waters lowest
+			double hp = 100.0;
+			int water = 0;
+			for (int i = 0; i < myTrees.length; i++) {
+				if ((double) myTrees[i].getHealth() < hp) {
+					hp = (double) myTrees[i].getHealth();
+					water = i;
+				}
+			}
+			rc.water(myTrees[water].getID());
+		}
+    }
 }
