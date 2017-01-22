2321708ea6f53c8287f37d4d6b5af624b7063975
nikhilsardana <2018nsardana@tjhsst.edu>
2017-01-22 11:48:37 -0500

    Add A* Pathfinding

--- segfaultplayer/Scout.java
+++ segfaultplayer/Scout.java
@@ -1,8 +1,9 @@
 package segfaultplayer;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.HashSet;
-
+import java.util.PriorityQueue;
 import java.util.Set;
 
 import battlecode.common.*;
@@ -25,12 +26,66 @@ public strictfp class Scout extends RobotBase
 		//circleEnemy(dank, dist);
 	}
 	
+	//== This Move class is used for pathfinding
+	class Move implements Comparable<Move> {
+		private float distanceToEnd;
+		private float totalCost; // heuristic
+		// the heuristic takes in the total number of moves (moves.size()) * the stride length) == the cost to reach here
+		// and the distanceToEnd*2 == the cost to goal (I multiplied by 2 so it works faster in straight line situations but tbh this is unnecessary)
+		private float stride;
+		private MapLocation location;
+		private ArrayList<Direction> moves;
+		// Constructor 1: When you have no moves
+		private Move(MapLocation thisLoc, MapLocation endLoc, float myStride) {
+			location = thisLoc;
+			distanceToEnd = thisLoc.distanceTo(endLoc);
+			moves = new ArrayList<Direction>();
+			stride = myStride;
+			totalCost = (moves.size() * stride) + (distanceToEnd*2);
+		}
+		// Constructor 2: When you have previous moves
+		private Move(MapLocation thisLoc, MapLocation endLoc, ArrayList<Direction> myMoves, float myStride) {
+			location = thisLoc;
+			distanceToEnd = thisLoc.distanceTo(endLoc);
+			moves = myMoves;
+			stride = myStride;
+			totalCost = (moves.size() * stride) + (distanceToEnd*2);
+		}
+		
+		private void updateList(Direction d) {
+			moves.add(d);
+			updateCost();
+		}
+		private float updateCost() {
+			totalCost = (moves.size() * stride) + (distanceToEnd*2);
+			return totalCost;
+		}
+		
+		private float getCost() {
+			return totalCost;
+		}
+		
+		public int compareTo(Move other) {
+			return Float.compare(totalCost,other.totalCost);
+		}	
+		
+		public String toString() {
+			int k = moves.size();
+			return (Float.toString(totalCost)+ "  " 
+			+ Float.toString(distanceToEnd) + "  " 
+			+ Float.toString((moves.size() * stride)) + " " 
+			+ Float.toString(stride) + " " 
+			+ Integer.toString(k));
+		}
+
+	}
+	
 	public void getpath(MapLocation el) throws GameActionException {
 		Set<Integer> uniqueTrees = new HashSet<Integer>();
 		float scaledNumTrees = 0;
 		int steps = 0;
 		float sR = rc.getType().sensorRadius;
-		float stride = 2.0f;
+		float stride = 1.0f;
 
 		System.out.println("dank Memes");
 		System.out.println(rc.getLocation().distanceTo(el));
@@ -51,20 +106,66 @@ public strictfp class Scout extends RobotBase
 			rc.broadcast(152, (int)scaledNumTrees);
 			
 			//pathfinding
-			Direction myDir = pathFind(myLoc, myLoc, el, myTrees, 30f, stride, sR);
-			if(myDir!=null) {
-				if(rc.canMove(myDir, stride)) {
-					rc.move(myDir, stride);
+			Move bestMove = pathMeme(myLoc, el, myTrees, 30f, stride, sR); // A*
+			//Direction myDir = pathFind(myLoc, myLoc, el, myTrees, 30f, stride, sR); // old pathfinding
+			if(bestMove!=null) {
+				if(rc.canMove(bestMove.moves.get(0), stride)) {
+					rc.move(bestMove.moves.get(0), stride);
 					steps+=1;
 					rc.broadcast(150, steps);
+					Clock.yield();
+				} else {
+					Clock.yield();
 				}
 			} else {
 				System.out.println("Theres NO PATH");
-				return;
+				Clock.yield();
 			}
-			Clock.yield();
 		}
 	}
+	// A* PathFinding
+	public Move pathMeme(MapLocation startLoc, MapLocation endLoc, TreeInfo[] trees, float theta, float stride, float rad) throws GameActionException {
+		
+		PriorityQueue<Move> myMoves = new PriorityQueue<Move>();
+		Move firstMove = new Move(startLoc, endLoc, stride);
+		myMoves.add(firstMove);
+		System.out.println("This is danker than my memes");
+		while(!myMoves.isEmpty()) {
+			Move lastMove = myMoves.remove();
+
+			MapLocation myLoc = lastMove.location;
+			
+			// ===== END CASE =====
+			if((startLoc.distanceTo(myLoc) + stride + 1) > rad) {
+				// end case -> we've made it this far and we can't sense much further
+				System.out.println("IM SO DANK");
+				return lastMove;
+			}
+			// ============================================================
+			// ==== add end case that returns no moves here =================
+			// ==== you don't have to wait for the entire queue to be over because it will never be ========
+			// ================================================================
+			
+			Direction[] possibleDirections = getDirections(myLoc, endLoc, theta);
+			for(Direction k : possibleDirections) {
+				MapLocation newLoc = myLoc.add(k, stride);
+				if(isClear(newLoc, trees)) {
+					//rc.setIndicatorLine(startLoc, newLoc, 0, 0, 255);
+					rc.setIndicatorLine(myLoc, newLoc, 0, 255, 0);
+					ArrayList<Direction> newList = new ArrayList<Direction>(lastMove.moves);
+					newList.add(k);
+					Move tempMove = new Move(newLoc, endLoc, newList, stride);
+					myMoves.add(tempMove);
+				} else {
+					rc.setIndicatorLine(myLoc, newLoc, 255, 0, 0);
+				}
+			}					
+
+		}
+		return null;
+		
+	}
+	// dfs pathfinding method
 	public Direction pathFind(MapLocation startLoc, MapLocation myLoc, MapLocation endLoc, TreeInfo[] trees, float theta, float stride, float rad) throws GameActionException {
 		if((startLoc.distanceTo(myLoc) + stride + 1) > rad) {
 			// end case -> we've made it this far and we can't sense much further
@@ -90,6 +191,8 @@ public strictfp class Scout extends RobotBase
 		return null;
 		
 	}
+	
+	// checks for nearby trees
 	public boolean isClear(MapLocation thisLoc, TreeInfo[] trees) {
 		for (TreeInfo t : trees) {
 			float distance = thisLoc.distanceTo(t.location);
@@ -99,6 +202,19 @@ public strictfp class Scout extends RobotBase
 		}
 		return true;
 	}
+	
+	// directions rotating around end goal
+    public static Direction[] getDirections(MapLocation myLoc, MapLocation otherLoc, float theta) throws GameActionException {
+    	Direction[] dirs = new Direction [(int)(360.0f/theta)];
+    	Direction bestdir = myLoc.directionTo(otherLoc);
+    	dirs[0] = bestdir;
+    	for (int j=1; j<dirs.length; j++) {
+    		bestdir = bestdir.rotateLeftDegrees(theta);
+    		dirs[j] = bestdir;
+    	}
+    	return dirs;
+	}
+    // directions towards end goal
     public static Direction[] getBestDirections(MapLocation myLoc, MapLocation otherLoc, float theta) throws GameActionException {
     	float initialtheta = theta;
     	Direction[] dirs = new Direction [(int)(360.0f/theta)];
