86d64fe142dc8965bfd5d98a0bfe3c66254bff59
Mihir Patel <mihir.v.patel7@gmail.com>
2017-01-29 11:24:59 -0500

    function update?

--- segfaultplayer/RobotBase.java
+++ segfaultplayer/RobotBase.java
@@ -529,7 +529,7 @@ public strictfp abstract class RobotBase
 	public void updateRatio(MapLocation myLocation) throws GameActionException {
 		int myRatio = (int)(3.0 / (1.0 + Math.pow(Math.E,3.0-2.0*(myLocation.distanceTo(enemyArchons[0])/myLocation.distanceTo(allyArchons[0]))) )*1000.0 );
 		int realRatio = rc.readBroadcast(11);
-		if(myRatio>realRatio) {
+		if(myRatio<realRatio) {
 			rc.broadcast(11, myRatio);
 		}
 	}
