0e7f98636b28aa7a39c6bff877e91efdafcf6773
Nikhil Sardana <2018nsardana@tjhsst.edu>
2017-01-28 22:42:41 -0500

    Fixed bullet-collecting scout

---
+++ segfaultplayer/Scout2.java
@@ -0,0 +1,88 @@
+package segfaultplayer;
+import java.util.HashSet;
+import java.util.Set;
+
+import battlecode.common.*;
+
+//
+public strictfp class Scout2 extends RobotBase
+{
+
+	public String alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+	public Scout2(RobotController rc, int id) throws GameActionException {
+		super(rc, id);
+	}
+
+	public void run() throws GameActionException {
+		MapLocation dank = rc.getInitialArchonLocations(enemy)[0];
+		System.out.println(dank);
+		bulletPath(dank);
+	}
+
+	public void bulletPath(MapLocation el) throws GameActionException {
+		Set<Integer> uniqueTrees = new HashSet<Integer>();
+		float scaledNumTrees = 0;
+		int steps = 0;
+		float sR = rc.getType().sensorRadius; // D Y N A M I C
+		float stride = rc.getType().strideRadius; // C O D E
+		System.out.println(rc.getLocation().distanceTo(el));
+		boolean isAtEnemy = false;
+		int counter = 0;
+		while (true) {
+			counter++;
+			Direction dir = rc.getLocation().directionTo(el);
+			if(rc.senseNearbyBullets().length>0) {
+				moveWithDodging(el);
+				Clock.yield();
+			} else {
+				MapLocation myLoc = rc.getLocation();
+				boolean hasShaken = false;
+				TreeInfo[] myTrees = rc.senseNearbyTrees(sR);
+				// going towards enemy archon when it doesn't have any bullets
+				if(!isAtEnemy) { 
+					isAtEnemy = rc.getLocation().distanceTo(el) < 10.0f;
+					float distance = myLoc.distanceTo(el);
+					//dir = myLoc.directionTo(el);
+				// go in random directions after reaching enemy archon
+				} else {
+					isAtEnemy=true;
+					if(counter%15==0) { // change direction
+						dir = randomDirection();
+					}
+				}
+				for (TreeInfo k : myTrees) {
+					float q = k.radius;
+					if(uniqueTrees.add(k.ID)) {
+						scaledNumTrees += (q*q);
+					}
+					if(k.containedBullets!=0) {
+						if(!hasShaken && rc.canShake(k.ID)){
+							rc.shake(k.ID);
+							//System.out.println("Got a bullet lolololol");
+							hasShaken = true;
+						} else {
+							dir = myLoc.directionTo(k.location);
+							break;
+						}
+					}
+				}
+	
+				//broadcast tree data
+				rc.broadcast(151, uniqueTrees.size());
+				rc.broadcast(152, (int)scaledNumTrees);
+				//move
+				if(rc.canMove(dir, stride)) {
+					rc.move(dir, stride);
+					steps+=1;
+					rc.broadcast(150, steps);
+					Clock.yield();
+				} else {
+					Clock.yield();
+				}
+			}
+		
+		}
+	}
+
+}
