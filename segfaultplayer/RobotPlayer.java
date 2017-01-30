463cd2657a0271577c66ae38e8c036bfe5f3831f
Neil Thistlethwaite <neilt1111@yahoo.com>
2017-01-29 19:11:06 -0500

    modify robotplayer to make firstgardener

--- segfaultplayer/RobotPlayer.java
+++ segfaultplayer/RobotPlayer.java
@@ -38,9 +38,13 @@ public strictfp class RobotPlayer {
     }
 
     public static void handleGardener(RobotController rc, int id) throws GameActionException {
-    	HexGardener hg = new HexGardener(rc, id);
-    	hg.run();
-
+    	if(id == 0) {
+    		FirstGardener fg = new FirstGardener(rc, id);
+    		fg.run();
+    	} else {
+    		HexGardener hg = new HexGardener(rc, id);
+    		hg.run();
+    	}
         //Gardener g = new Gardener(rc, id);
         //g.run();
         //g.runAlternate(RobotType.SCOUT);
