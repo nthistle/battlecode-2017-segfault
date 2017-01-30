d1f05ed91297e993d36627d74d5729e3a5fcf2a2
Neil Thistlethwaite <neilt1111@yahoo.com>
2017-01-29 19:08:12 -0500

    finish FirstGardener

--- segfaultplayer/FirstGardener.java
+++ segfaultplayer/FirstGardener.java
@@ -1,5 +1,6 @@
 package segfaultplayer;
 import java.util.ArrayList;
+import java.util.LinkedList;
 
 import battlecode.common.*;
 
@@ -69,8 +70,11 @@ public strictfp class FirstGardener extends HexGardener
 		
 		if(numFree <= 1) {
 			caseClosed();
+		} else if(rc.readBroadcast(2) == 2) {
+			caseFar();
+		} else {
+			caseNear();
 		}
-		
 		
 		/*
 		
@@ -186,6 +190,163 @@ public strictfp class FirstGardener extends HexGardener
 		}*/
 	}
 	
+	public void caseClosed() throws GameActionException {
+		System.out.println("CASE: CLOSED");
+		LinkedList<Order> myOrders = new LinkedList<Order>();
+		myOrders.addLast(new Order(OrderType.ROBOT, RobotType.LUMBERJACK));
+		myOrders.addLast(new Order(OrderType.ROBOT, RobotType.SOLDIER));
+		myOrders.addLast(new Order(OrderType.TREE));
+		myOrders.addLast(new Order(OrderType.ROBOT, RobotType.SCOUT));
+		myOrders.addLast(new Order(OrderType.TREE));
+		myOrders.addLast(new Order(OrderType.TREE));
+		while(myOrders.size() > 0) {
+			checkVPWin();
+			updatePodStatus();
+			drawPodStatus();
+			
+			Order nextOrder = myOrders.peekFirst();
+			if(nextOrder.type == OrderType.TREE) {
+				if(isAbleToBuildTree()) {
+					if(addTreeToPod()) {
+						System.out.println("We succesfully added our tree");
+						myOrders.pollFirst();
+					}
+				}
+			} else {
+				if(buildCooldown <= 0) {
+					if(rc.getTeamBullets() > nextOrder.rt.bulletCost) {
+						Direction buildDir = getBuildDirection(nextOrder.rt);
+						if(buildDir == null) {
+							// can't build this, ohwell
+						} else {
+							rc.buildRobot(nextOrder.rt, buildDir);
+							System.out.println("We succesfully built a " + nextOrder.rt.toString());
+							buildCooldown = 15;
+						}
+					}
+				}
+			}
+			
+			waterLowest();
+			if(buildCooldown>0) buildCooldown--;
+			Clock.yield();
+		}
+		
+		weNeedAnotherGardener();
+		
+		// TODO: make this main loop a little more sophisticated
+		while(true) {
+			checkVPWin();
+			waterLowest();
+			Clock.yield();
+		}
+		
+	}
+	
+	public void caseFar() throws GameActionException {
+		System.out.println("CASE: FAR");
+		LinkedList<Order> myOrders = new LinkedList<Order>();
+		myOrders.addLast(new Order(OrderType.ROBOT, RobotType.SOLDIER));
+		myOrders.addLast(new Order(OrderType.ROBOT, RobotType.SCOUT));
+		myOrders.addLast(new Order(OrderType.TREE));
+		myOrders.addLast(new Order(OrderType.TREE));
+		while(myOrders.size() > 0) {
+			checkVPWin();
+			updatePodStatus();
+			drawPodStatus();
+			
+			Order nextOrder = myOrders.peekFirst();
+			if(nextOrder.type == OrderType.TREE) {
+				if(isAbleToBuildTree()) {
+					if(addTreeToPod()) {
+						System.out.println("We succesfully added our tree");
+						myOrders.pollFirst();
+					}
+				}
+			} else {
+				if(buildCooldown <= 0) {
+					if(rc.getTeamBullets() > nextOrder.rt.bulletCost) {
+						Direction buildDir = getBuildDirection(nextOrder.rt);
+						if(buildDir == null) {
+							// can't build this, ohwell
+						} else {
+							rc.buildRobot(nextOrder.rt, buildDir);
+							System.out.println("We succesfully built a " + nextOrder.rt.toString());
+							buildCooldown = 15;
+						}
+					}
+				}
+			}
+			
+			waterLowest();
+			if(buildCooldown>0) buildCooldown--;
+			Clock.yield();
+		}
+		
+		weNeedAnotherGardener();
+		
+		// TODO: make this main loop a little more sophisticated
+		while(true) {
+			checkVPWin();
+			waterLowest();
+			Clock.yield();
+		}
+	}
+	
+	public void caseNear() throws GameActionException {
+		System.out.println("CASE: NEAR");
+		LinkedList<Order> myOrders = new LinkedList<Order>();
+		myOrders.addLast(new Order(OrderType.ROBOT, RobotType.SOLDIER));
+		myOrders.addLast(new Order(OrderType.ROBOT, RobotType.SOLDIER));
+		myOrders.addLast(new Order(OrderType.TREE));
+		myOrders.addLast(new Order(OrderType.TREE));
+		myOrders.addLast(new Order(OrderType.TREE));
+		while(myOrders.size() > 0) {
+			checkVPWin();
+			updatePodStatus();
+			drawPodStatus();
+			
+			Order nextOrder = myOrders.peekFirst();
+			if(nextOrder.type == OrderType.TREE) {
+				if(isAbleToBuildTree()) {
+					if(addTreeToPod()) {
+						System.out.println("We succesfully added our tree");
+						myOrders.pollFirst();
+					}
+				}
+			} else {
+				if(buildCooldown <= 0) {
+					if(rc.getTeamBullets() > nextOrder.rt.bulletCost) {
+						Direction buildDir = getBuildDirection(nextOrder.rt);
+						if(buildDir == null) {
+							// can't build this, ohwell
+						} else {
+							rc.buildRobot(nextOrder.rt, buildDir);
+							System.out.println("We succesfully built a " + nextOrder.rt.toString());
+							buildCooldown = 15;
+						}
+					}
+				}
+			}
+			
+			waterLowest();
+			if(buildCooldown>0) buildCooldown--;
+			Clock.yield();
+		}
+		
+		weNeedAnotherGardener();
+		
+		// TODO: make this main loop a little more sophisticated
+		while(true) {
+			checkVPWin();
+			waterLowest();
+			Clock.yield();
+		}
+	}
+
+	public void weNeedAnotherGardener() throws GameActionException {
+		rc.broadcast(21, 1);
+	}
 	
 	public void findFirstPodLocation() throws GameActionException {
 		this.findPodLocation();
