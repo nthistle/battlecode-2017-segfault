4fa02873f082a3bfffbad3bd55c54daf04b1d6a0
nikhilsardana <2018nsardana@tjhsst.edu>
2017-01-30 19:51:44 -0500

    Fix scout and lj / soldier ratio

--- segfaultplayer/Scout3.java
+++ segfaultplayer/Scout3.java
@@ -28,7 +28,9 @@ public strictfp class Scout3 extends RobotBase
 		boolean isAtEnemy = false;
 		int counter = 0;
 		Direction dir = rc.getLocation().directionTo(el);
+		MapLocation target = el;
 		while (true) {
+			System.out.println("DANKDNAKNDAK");
 			counter++;
 			if(rc.senseNearbyBullets(5f).length>0) {
 				moveWithDodging(el);
@@ -38,11 +40,10 @@ public strictfp class Scout3 extends RobotBase
 				boolean hasShaken = false;
 				TreeInfo[] myTrees = rc.senseNearbyTrees(sR);
 				// going towards enemy archon when it doesn't have any bullets
-				/*
-				if(!isAtEnemy) {
-					isAtEnemy = rc.getLocation().distanceTo(el) < 10.0f;
-					float distance = myLoc.distanceTo(el);
-					//dir = myLoc.directionTo(el);
+				
+				if (!isAtEnemy) {
+					isAtEnemy = rc.getLocation().distanceTo(el) < rc.getLocation().distanceTo(allyArchons[0]);
+					dir = myLoc.directionTo(el);
 					// go in random directions after reaching enemy archon
 				} else {
 					isAtEnemy=true;
@@ -53,17 +54,13 @@ public strictfp class Scout3 extends RobotBase
 						}
 					}
 				}
-				*/
-				if(counter%15==0) { // change direction
-					dir = randomDirection();
-					while(!rc.canMove(dir)) {
-						dir = randomDirection();
-					}
-				}
+				
 				for (TreeInfo k : myTrees) {
 					float q = k.radius;
 					if(uniqueTrees.add(k.ID)) {
-						scaledNumTrees += (q*q);
+						if(!isAtEnemy) {
+							scaledNumTrees += (q*q);
+						}
 					}
 					if(k.containedBullets!=0) {
 						if(!hasShaken && rc.canShake(k.ID)){
@@ -71,21 +68,43 @@ public strictfp class Scout3 extends RobotBase
 							hasShaken = true;
 						} else {
 							dir = myLoc.directionTo(k.location);
+							target = k.location;
 							break;
 						}
 					}
 				}
 
 				//broadcast tree data
-				rc.broadcast(151, uniqueTrees.size());
-				rc.broadcast(152, (int)scaledNumTrees);
+				if(!isAtEnemy) {
+					rc.broadcast(151, uniqueTrees.size());
+					rc.broadcast(152, (int)scaledNumTrees);
+				}
 				//move
 				if(rc.canMove(dir, stride)) {
 					rc.move(dir, stride);
-					steps+=1;
-					rc.broadcast(150, steps);
+					if(!isAtEnemy) {
+						steps+=1;
+						rc.broadcast(150, steps);
+					}
+					if(rc.getRoundNum() < 100) {
+						float broadValue = 
+							(uniqueTrees.size() / (steps*(float)Math.PI)) 
+							* (allyArchons[0].distanceTo(el) / 100f)
+							* (1f / ((float)rc.readBroadcast(12)/1000f));
+						int val = (int)(broadValue*1000f);
+						rc.broadcast(13, val);
+					} else {
+						float broadValue = 
+								(uniqueTrees.size() / (steps*(float)Math.PI)) 
+								* (allyArchons[0].distanceTo(el) / 100f)
+								* (1f / ((float)rc.readBroadcast(12)/1000f))
+								* (100f / (float)rc.getRoundNum());
+						int val = (int)(broadValue*1000f);
+						rc.broadcast(13, val);
+					}
 					Clock.yield();
 				} else {
+					pathFind(target);
 					Clock.yield();
 				}
 			}
